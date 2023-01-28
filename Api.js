09d113e35ef1a3d3d1a1ceacaacd179ffc177933
arkaitzgutierrez <arkaitzgutierrez@gmail.com>
2023-01-28 10:11:34 +0100

    Primer cambio fin de semana

--- Api.js
+++ Api.js
@@ -1,13 +1,13 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '92b7670a43mshd81f13f9ca76ee3p1c9fa9jsn4e09fa2d29ef',
-		'X-RapidAPI-Host': 'youtube-music1.p.rapidapi.com'
-	}
-};
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '92b7670a43mshd81f13f9ca76ee3p1c9fa9jsn4e09fa2d29ef',
+// 		'X-RapidAPI-Host': 'youtube-music1.p.rapidapi.com'
+// 	}
+// };
 
-fetch('https://youtube-music1.p.rapidapi.com/v2/search?query=bryan%20adams', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// fetch('https://youtube-music1.p.rapidapi.com/v2/search?query=bryan%20adams', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
     
