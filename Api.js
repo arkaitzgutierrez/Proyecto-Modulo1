98f8c6d9037719d65d2ab4653e66658ad081c4b1
Garagorri <famgardazu@gmail.com>
2023-01-30 09:51:50 +0100

    Pido cambios

--- Api.js
+++ Api.js
@@ -1,35 +1,33 @@
-
 let url = "https://youtube-music1.p.rapidapi.com/v2/search?query=";
-datosArtista(url);
-function datosArtista(url) {
-  document
-    .querySelector("input")
-    .addEventListener("keypress", function () {
-      let artista = document.querySelector("#etiqBus").value;
+// datosArtista(url);
+// function datosArtista(url) {
+// }
 
-      let peticionAYoutube = (url + artista);
-      
-    });
+document.querySelector("input").addEventListener("keyup", function () {
+  let artista = document.querySelector("#etiqBus").value;
   let options = {
     method: "GET",
     headers: {
       "X-RapidAPI-Key": "92b7670a43mshd81f13f9ca76ee3p1c9fa9jsn4e09fa2d29ef",
       "X-RapidAPI-Host": "youtube-music1.p.rapidapi.com",
-      
     },
   };
 
-  fetch(peticionAYoutube, options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
+  fetch(url + artista, options)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+
+      for (let i = 0; i < res.results.length; i++) {
+        let id = res.results[i].id;
+
+        console.log(results);
+
+        document.querySelector("#Resultados").innerHTML += `
+      
+      `;
+      }
+    })
     .catch((err) => console.error(err));
-
-  for (let i = 0; i < response.results.length; i++) {
-    let videos = response.results[i].videos;
-
-    document.querySelector("select").innerHTML += `
-        <option>${response.results[i].videos}</option>
-            `;
-  }
-  datosArtista(url);
-}
+  // datosArtista(url);
+});
